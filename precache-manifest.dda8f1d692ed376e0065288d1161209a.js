d468a45c79863641cb1b67c01632a33bae5749e7
yyuri <oriol.goma@gmail.com>
2019-12-29 13:18:54 +0100

    Updates

---
+++ precache-manifest.dda8f1d692ed376e0065288d1161209a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5de15af855414d533d83eaec04f2f2",
+    "url": "/salvemelcalamot/index.html"
+  },
+  {
+    "revision": "da12088db65a93da615a",
+    "url": "/salvemelcalamot/static/css/main.77a45694.chunk.css"
+  },
+  {
+    "revision": "b00288b1c4007d44715f",
+    "url": "/salvemelcalamot/static/js/2.0205871d.chunk.js"
+  },
+  {
+    "revision": "cccbdbb00528ba3f2c976f87938d383c",
+    "url": "/salvemelcalamot/static/js/2.0205871d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da12088db65a93da615a",
+    "url": "/salvemelcalamot/static/js/main.2c462533.chunk.js"
+  },
+  {
+    "revision": "2063a58abdc9e85394ce",
+    "url": "/salvemelcalamot/static/js/runtime-main.7f1486ce.js"
+  },
+  {
+    "revision": "025124a84f85680db61ea5ee15072285",
+    "url": "/salvemelcalamot/static/media/aeri.025124a8.jpg"
+  },
+  {
+    "revision": "518019c61e3919cc6ba67580a22d3a52",
+    "url": "/salvemelcalamot/static/media/aiguesverdes.518019c6.jpg"
+  },
+  {
+    "revision": "ef3755bc8c4d83509102ca7f8c0ebfcb",
+    "url": "/salvemelcalamot/static/media/ajudesbanca.ef3755bc.png"
+  },
+  {
+    "revision": "936113fc288c199bde7e7c7e694be5c0",
+    "url": "/salvemelcalamot/static/media/algarrobo.936113fc.jpg"
+  },
+  {
+    "revision": "b220c5aa1d0ee9cbe7f8e5437c76c788",
+    "url": "/salvemelcalamot/static/media/biciclista.b220c5aa.jpeg"
+  },
+  {
+    "revision": "2f1c38e41bd827a7495b15c13aceb99c",
+    "url": "/salvemelcalamot/static/media/biciclistas.2f1c38e4.jpeg"
+  },
+  {
+    "revision": "038b6a412805eca28cffabb7c1f44282",
+    "url": "/salvemelcalamot/static/media/calamothead.038b6a41.jpg"
+  },
+  {
+    "revision": "d4bdbaaee90faacdd9c77b0927c8fbdd",
+    "url": "/salvemelcalamot/static/media/canyars.d4bdbaae.jpg"
+  },
+  {
+    "revision": "29b236d7014e92a5527765d1385d4708",
+    "url": "/salvemelcalamot/static/media/carreteres.29b236d7.jpg"
+  },
+  {
+    "revision": "2c278d2073839df2b4d09465b7f84053",
+    "url": "/salvemelcalamot/static/media/connector.2c278d20.jpg"
+  },
+  {
+    "revision": "3e0345880f4bef865d3a309bc0f82cce",
+    "url": "/salvemelcalamot/static/media/contaminacio.3e034588.jpg"
+  },
+  {
+    "revision": "cf9e5a2aac480c5354a28548b844030f",
+    "url": "/salvemelcalamot/static/media/contaminacio1.cf9e5a2a.jpg"
+  },
+  {
+    "revision": "9610bb2833e449497ddd023ffdaeab96",
+    "url": "/salvemelcalamot/static/media/exploracio.9610bb28.jpg"
+  },
+  {
+    "revision": "2c1883745d7d305ad0c988746a6b9cef",
+    "url": "/salvemelcalamot/static/media/felip.2c188374.gif"
+  },
+  {
+    "revision": "107d41b6f5a693a9dad6868cf3003baa",
+    "url": "/salvemelcalamot/static/media/ipla3.107d41b6.jpg"
+  },
+  {
+    "revision": "d92c7154ebaa9b0b7211063441a7fdaf",
+    "url": "/salvemelcalamot/static/media/mapsriera.d92c7154.png"
+  },
+  {
+    "revision": "b7c38b89deb63117651836f32b7af01c",
+    "url": "/salvemelcalamot/static/media/paseante.b7c38b89.jpeg"
+  },
+  {
+    "revision": "f4fcf73fee2e502c1c4b6e7b781db517",
+    "url": "/salvemelcalamot/static/media/paseantes.f4fcf73f.jpeg"
+  },
+  {
+    "revision": "ea2776afd8db5ce18fff7cc7b9a2e449",
+    "url": "/salvemelcalamot/static/media/pi.ea2776af.jpg"
+  },
+  {
+    "revision": "66478d943a334c452ea80dbce6052f56",
+    "url": "/salvemelcalamot/static/media/pi2.66478d94.jpg"
+  },
+  {
+    "revision": "ecb91d506ab3c6d5a88130af5bcdb8bf",
+    "url": "/salvemelcalamot/static/media/pladeponent.ecb91d50.jpg"
+  },
+  {
+    "revision": "0e3f8295db2393cb1a84488f8dce9c69",
+    "url": "/salvemelcalamot/static/media/pladeponent2.0e3f8295.jpg"
+  },
+  {
+    "revision": "408ebaad370512e172b86a0c892bf923",
+    "url": "/salvemelcalamot/static/media/riera.408ebaad.jpeg"
+  },
+  {
+    "revision": "7331f48046c68df07151ba3400d49aea",
+    "url": "/salvemelcalamot/static/media/riera.7331f480.jpg"
+  },
+  {
+    "revision": "26ca681e5b0ea5119ef0de5144e4731b",
+    "url": "/salvemelcalamot/static/media/riera2.26ca681e.jpg"
+  },
+  {
+    "revision": "c20f1e2fb89021d135bca2d56e1e990a",
+    "url": "/salvemelcalamot/static/media/rieramap.c20f1e2f.jpg"
+  },
+  {
+    "revision": "5673d6ac3d689de4c8d5f8a085a09fef",
+    "url": "/salvemelcalamot/static/media/vayariera.5673d6ac.jpg"
+  },
+  {
+    "revision": "82fd749d48fbb6e26d983669f0b5a71b",
+    "url": "/salvemelcalamot/static/media/vertix.82fd749d.jpg"
+  }
+]);
